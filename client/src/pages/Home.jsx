66cff3765be285d3f3786355a7d79e6f96984795
vishnu <vishnuthambankv@gmail.com>
2025-03-22 22:56:20 +0530

    upload file start

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       home page
+      {/* message component */}
+      <section>
+        <Outlet/>
+      </section>
     </div>
   )
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoCloseOutline } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+import uploadFile from '../helpers/uploadFile';
 
 const RegisterPage = () => {
+
+  const [data, setData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    profile_pic:''
+  })
+
+  const [uploadPhoto, setUploadPhoto] = useState("")
+
+  const handleOnChange = (e) => {
+    const {name, value} = e.target
+
+    setData((prev) => {
+      return {
+        ...prev,
+        [name] : value
+      }
+    })
+  }
+
+  const handleUploadPhoto = async (e) => {
+    const file = e.target.files[0]
+    const uploadPhoto = await uploadFile(file)
+    setUploadPhoto(file)
+  }
+
+  const handleClearUploadPhoto = (e) => {
+    e.stopPropagation()
+    e.preventDefault()
+    setUploadPhoto(null)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
   return (
-    <div>
-      register page
+    <div className='mt-5'>
+      <div className='bg-white w-full max-w-md mx-2 rounded overflow-hidden p-4 md:mx-auto'>
+        <h3>Welcome to Chat App</h3>
+
+        <form action="" className='grid gap-4 mt-5' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' name='name' placeholder='enter your name' className='bg-slate-100 px-2 py-1 focus:outline-[#00acb4]' value={data.name} onChange={handleOnChange} required />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' placeholder='enter your email' className='bg-slate-100 px-2 py-1 focus:outline-[#00acb4]' value={data.email} onChange={handleOnChange} required />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' name='password' placeholder='enter your password' className='bg-slate-100 px-2 py-1 focus:outline-[#00acb4]' value={data.password} onChange={handleOnChange} required />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="profile_pic">Photo
+
+              <div className='h-14 bg-slate-200 flex justify-center items-center border border-slate-200 rounded hover:border-[#00acb4] cursor-pointer'>
+                <p className='text-sm max-w-[300px] text-ellipsis line-clamp-1'>
+                  {
+                    uploadPhoto?.name ? uploadPhoto ?.name : "Upload profile Photo"
+                  }
+                </p>
+                {
+                  uploadPhoto?.name && (
+                    <button className='text-lg ml-2 hover:text-red-600 cursor-pointer' onClick={handleClearUploadPhoto}>
+                      <IoCloseOutline/>
+                    </button>
+                  )
+                }
+              </div>
+            </label>
+            
+            <input type="file" id='profile_pic' name='profile_pic' className='bg-slate-100 px-2 py-1 focus:outline-[#00acb4] hidden' onChange={handleUploadPhoto} />
+          </div>
+
+          <button className='bg-[#00acb4] text-lg px-4 py-1 hover:bg-[#058187] rounded mt-2 font-bold text-white leading-relaxed tracking-wide'>Register</button>
+        </form>
+
+        <p className='my-3 text-center'>Already have an account ? <Link to={'/email'} className='hover:text-[#00acb4] font-semibold'>Login</Link></p>
+      </div>
     </div>
   )
 }
